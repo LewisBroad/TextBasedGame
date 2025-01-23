8b832441ad96e6b17ca2aa9f3d43886d0cabe048
Lewis Broad <lewisbroad@live.com>
2025-01-23 10:31:04 +0000

    Added comment

--- TextBasedGame/ExceptionHandler.h
+++ TextBasedGame/ExceptionHandler.h
@@ -8,14 +8,14 @@ class ExceptionHandler:public exception
 protected:
 	string message;
 public:
-	explicit ExceptionHandler(string& message) : message(message) {}
-	const char * what(){
+	explicit ExceptionHandler(const string& message) : message(message) {}
+	const char* what() const override { //override the what function so that other classes can use it
 		return message.c_str();
 	}
 };
 class invalidActionExeption :public ExceptionHandler
 {
 public:
-	invalidActionExeption() :ExceptionHandler("Invalid action") {}
+	invalidActionExeption() :ExceptionHandler("Invalid action") {} 
 };
 
