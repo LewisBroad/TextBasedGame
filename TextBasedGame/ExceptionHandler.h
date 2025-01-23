71c657a59f99d036110461b071fa33de5fba9d99
Lewis Broad <lewisbroad@live.com>
2025-01-23 10:14:36 +0000

    Update ExceptionHandler.h
    Altered the exception handler class

--- TextBasedGame/ExceptionHandler.h
+++ TextBasedGame/ExceptionHandler.h
@@ -8,8 +8,8 @@ class ExceptionHandler:public exception
 protected:
 	string message;
 public:
-	explicit ExceptionHandler(const string& message) : message(message) {}
-	const char* what() const noexcept override {
+	explicit ExceptionHandler(string& message) : message(message) {}
+	const char * what(){
 		return message.c_str();
 	}
 };
